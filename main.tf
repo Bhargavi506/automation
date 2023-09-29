ca8c9966903ba6d11da6b374fb38e4248fa668a6
Bhargavi506 <bhargaviborra506@gmail.com>
2023-09-29 15:00:16 +0530

    create s3 bucket

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket" "terraform-backend-s3-bucket-123456" {
   versioning {
     enabled = true
   }
-  server_uside_encryption_configuration {
+  server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm= "AES256"
