5a1f43f976b316233d4e409a2d821f978793d14e
Bhargavi506 <bhargaviborra506@gmail.com>
2023-09-29 14:58:55 +0530

    create s3 bucket

--- main.tf
+++ main.tf
@@ -4,9 +4,8 @@ provider "aws" {
     #secret_key = ""
 }
 # create s3 bucket
-resource "aws_s3_bucket" "terraform-backend-s3-bucket-12345" {
-  bucket = "terraform-backend-s3-bucket-12345" # Replace with your desired bucket name
-<<<<<<< HEAD
+resource "aws_s3_bucket" "terraform-backend-s3-bucket-123456" {
+  bucket = "terraform-backend-s3-bucket-123456" # Replace with your desired bucket name
   acl    = "private"               # You can configure the access control list (ACL) as needed
 
   lifecycle {
@@ -22,12 +21,8 @@ resource "aws_s3_bucket" "terraform-backend-s3-bucket-12345" {
       }
     }
   }
-=======
-  acl    = "public"               # You can configure the access control list (ACL) as needed
-
->>>>>>> 5fd778035c6edfca0d45345afee26c14a37d4e16
   tags = {
-    Name        = "terraform-backend-s3-bucket-12345"
+    Name        = "terraform-backend-s3-bucket-123456"
   }
 }
 
