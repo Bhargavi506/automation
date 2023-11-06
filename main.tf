eaae77f2c2f05122f8bb58c30e7cb715c5d610d0
Bhargavi Borra <82596888+Bhargavi506@users.noreply.github.com>
2023-11-06 16:55:54 +0530

    remove  s3 bucket creation

--- main.tf
+++ main.tf
@@ -3,36 +3,7 @@ provider "aws" {
     #access_key =""
     
 }
-# create s3 bucket
-resource "aws_s3_bucket" "terraform_state_s3" {
-  #make sure you give unique bucket name
-  bucket = "terraform-state-s3-111" 
-  force_destroy = true
-# Enable versioning to see full revision history of our state files
-  versioning {
-         enabled = true
-        }
 
-# Enable server-side encryption by default
-server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-# 2 - this Creates Dynamo Table
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "tf-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-        attribute {
-         name = "LockID"
-         type = "S"
-      }
-}
 # giving backend state file management with s3
 terraform {
   backend "s3" {
